fadd0a07a0c413adc27607b93ac0e7bf0b4c2caf
Welbert Serra <welberts@gmail.com>
2016-09-24 09:22:22 -0300

    Criação dos search e filter das interfaces de cadastro mais simples

--- registro/admin.py
+++ registro/admin.py
@@ -16,8 +16,25 @@ class ReservaAdmin(admin.ModelAdmin):
     list_display = ('sigla', 'descricao')
     search_fields = ['sigla','descricao']
 
+class MunicaoAdmin(admin.ModelAdmin):
+    fields = ['descricao','calibre']
+    list_display = ('descricao','calibre')
+    list_filter = ['calibre']
+    search_fields = ['descricao','calibre']
+
+class AcessorioAdmin(admin.ModelAdmin):
+    fields = ['descricao']
+    search_fields = ['descricao']
+
+class MilitarAdmin(admin.ModelAdmin):
+    fields = ['posto','nome_de_guerra']
+    list_display = ('posto','nome_de_guerra')
+    search_fields = ['posto','nome_de_guerra']
+    list_filter = ['posto']
+
+
 admin.site.register(Armamento,ArmamentoAdmin)
 admin.site.register(Reserva,ReservaAdmin)
-admin.site.register(Municao)
-admin.site.register(Acessorio)
-admin.site.register(Militar)
+admin.site.register(Municao,MunicaoAdmin)
+admin.site.register(Acessorio,AcessorioAdmin)
+admin.site.register(Militar,MilitarAdmin)

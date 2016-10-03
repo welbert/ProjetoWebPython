4735f69aafb86a8940a9a19883d93fa7a31ec9d8
Welbert Serra <welberts@gmail.com>
2016-10-02 22:44:23 -0300

    Models OK, FALTA, Tratar a relação manytomany na pagina admin e tratar as regras de negócio

--- README.md
+++ README.md
@@ -30,22 +30,21 @@ Pass: virtualbox
 
 
 
-> # TODO 
+> # TODO
 
 - [ ] Cronograma
 - [ ] Material Escrito
   - [ ] Capa
-  - [ ] Contracapa com tema, equipe, dados da disciplina e demais informações pertinentes ao tema. 
-  - [x] Diagrama de Classes
-  - [x] Entidade e Relacionamento 
+  - [ ] Contracapa com tema, equipe, dados da disciplina e demais informações pertinentes ao tema.
+  - [ ] Diagrama de Classes `REVISAR - Retirar a classe produto`
+  - [ ] Entidade e Relacionamento `REVISAR - Retirar a classe produto`
 - [ ] Slides
 - [x] *Login/Logout*
 - [x] *Menu de opções*
-- [ ] **Tela(Simples)** *Usuário* (nome completo, login, senha, email);
-- [ ] **Tela(Simples)** *Armamento* (nº de série, modelo, fabricante);
-- [ ] **Tela(Simples)** *Acessórios* (descrição);
-- [ ] **Tela(Simples)** *Munição* (calibre, descrição);
-- [ ] **Tela(Simples)** *Produto* ('Armamento/Acessórios/Munição'[Classe Genérica]);
+- [x] **Tela(Simples)** *Usuário* (nome completo, login, senha, email);
+- [x] **Tela(Simples)** *Armamento* (nº de série, modelo, fabricante);
+- [x] **Tela(Simples)** *Acessórios* (descrição);
+- [x] **Tela(Simples)** *Munição* (calibre, descrição);
 - [ ] **Tela(Simples)** *Reserva* (sigla, descrição);
 - [ ] **Tela(Simples)** *Militar* (posto/graduação, nome de guerra);
 - [ ] **Tela(Integração)** *ReservaMaterial* (cadastro de armamento, munição e acessórios com suas respectivas quantidades na reserva de armamento)
@@ -53,4 +52,3 @@ Pass: virtualbox
 - [ ] **Regra(Negócio)** Um militar da reserva de armamento cadastra todos os armamentos, munições e acessórios (lanterna, mascara de gás,coldre, bandoleira, etc) e em seguida (em Manter Reserva Material) informa quanto ele tem de cada equipamento na reserva.
 - [ ] **Regra(Negócio)** Um outro militar pode se dirigir a reserva de armamento para cautelar armamento(s), munição e/ou acessórios. Nesse momento o militar da reserva registra essa retirada (Manter Cautela) informando o militar e tudo que ele está retirando, nesse momento é feito um registro com data, hora, militar responsável e todo o material retirado.
 - [ ] **Regra(Negócio)** Quando o militar retorna a reserva de armamento para devolver o material, é dado a baixa no equipamento. Esse material pode ser devolvido aos poucos.
-
--- registro/models.py
+++ registro/models.py
@@ -1,32 +1,74 @@
 from django.db import models
 
-
 class Armamento(models.Model):
-    numero_de_serie = models.IntegerField(default=0)
-    modelo = models.CharField(max_length=200)
-    fabricante = models.CharField(max_length=200)
+    numero_de_serie = models.IntegerField('Numero de Serie',default=0)
+    modelo = models.CharField('Modelo',max_length=200)
+    fabricante = models.CharField('Fabricante',max_length=200)
     def __str__(self):
         return "Numero de Serie: "+str(self.numero_de_serie)+" - Modelo: "+self.modelo+" - Fabricante: "+self.fabricante
 
-class Reserva(models.Model):
-    sigla = models.CharField(max_length=5)
-    descricao = models.CharField(max_length=250)
-    def __str__(self):
-        return "Sigla: "+self.sigla+" - Descricao: "+self.descricao
 
 class Municao(models.Model):
-    calibre = models.DecimalField(max_digits=5, decimal_places=2)
-    descricao = models.CharField(max_length=250)
+    calibre = models.DecimalField('Calibre',max_digits=5, decimal_places=2)
+    descricao = models.CharField('Descricao',max_length=250)
     def __str__(self):
         return "Calibre: "+str(self.calibre)+" - Descricao: "+self.descricao
 
 class Acessorio(models.Model):
-    descricao = models.CharField(max_length=250)
+    descricao = models.CharField('Descricao',max_length=250)
     def __str__(self):
         return self.descricao
 
+class Reserva_Armamento(models.Model):
+    armamento = models.ForeignKey('Armamento')
+    reserva = models.ForeignKey('Reserva')
+    quantidade = models.FloatField('Quantidade')
+
+class Reserva_Municao(models.Model):
+    municao = models.ForeignKey('Municao')
+    reserva = models.ForeignKey('Reserva')
+    quantidade = models.FloatField('Quantidade')
+
+class Reserva_Acessorio(models.Model):
+    acessorio = models.ForeignKey('Acessorio')
+    reserva = models.ForeignKey('Reserva')
+    quantidade = models.FloatField('Quantidade')
+
+class Reserva(models.Model):
+    sigla = models.CharField('Sigla',max_length=5)
+    descricao = models.CharField('Descricao',max_length=250)
+    armamento = models.ManyToManyField(Armamento, through="Reserva_Armamento")
+    municao = models.ManyToManyField(Municao, through="Reserva_Municao")
+    acessorio = models.ManyToManyField(Acessorio, through="Reserva_Acessorio")
+    def __str__(self):
+        return "Sigla: "+self.sigla+" - Descricao: "+self.descricao
+
+class Cautela_Armamento(models.Model):
+    armamento = models.ForeignKey('Armamento')
+    cautela = models.ForeignKey('Cautela')
+    quantidade = models.FloatField('Quantidade')
+
+class Cautela_Municao(models.Model):
+    municao = models.ForeignKey('Municao')
+    cautela = models.ForeignKey('Cautela')
+    quantidade = models.FloatField('Quantidade')
+
+class Cautela_Acessorio(models.Model):
+    acessorio = models.ForeignKey('Acessorio')
+    cautela = models.ForeignKey('Cautela')
+    quantidade = models.FloatField('Quantidade')
+
+class Cautela(models.Model):
+    data_de_retirada = models.DateTimeField('Data de Retirada',auto_now_add=True)
+    armamento = models.ManyToManyField(Armamento, through="Cautela_Armamento")
+    municao = models.ManyToManyField(Municao, through="Cautela_Municao")
+    acessorio = models.ManyToManyField(Acessorio, through="Cautela_Acessorio")
+    def __str__(self):
+        return str(self.data_de_retirada)
+
 class Militar(models.Model):
-    posto = models.CharField(max_length=60)
-    nome_de_guerra = models.CharField(max_length=120)
+    posto = models.CharField('Posto',max_length=60)
+    nome_de_guerra = models.CharField('Nome de Guerra',max_length=120)
+    cautela = models.ManyToManyField(Cautela)
     def __str__(self):
         return "Posto: "+self.posto+" - Nome de Guerra: "+self.nome_de_guerra

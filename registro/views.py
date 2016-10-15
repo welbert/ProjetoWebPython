09fa581bcb404db8eaec78d3e2fba8b87eb2c017
dnovaes <novaesdigo@gmail.com>
2016-10-15 19:53:59 -0300

    template de cautelar atualizar com submenu. cautelar primeira versão funcional. urls adequada com a nova template de cautelas.

--- registro/views.py
+++ registro/views.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render, HttpResponse
-from .models import Armamento, Municao, Acessorio, Militar, Reserva
+from .models import Armamento, Municao, Acessorio, Militar, Reserva, Cautela_Armamento, Cautela_Municao, Cautela_Acessorio, Cautela_Militar, Reserva_Armamento, Reserva_Municao, Reserva_Acessorio
 from django.contrib.auth.decorators import login_required
 from django.template import loader
-
+from django.db import transaction
+from datetime import datetime
 # Create your views here.
 
 @login_required(login_url="../admin/login/")
@@ -124,7 +125,7 @@ def cautelar_index(request):
 
     #militar = models.ForeignKey('Militar',blank=True,null=True)
     militar_resp = Militar.objects.raw("SELECT * FROM registro_militar WHERE user_id = "+str(request.user.id))[0]
-    reserva = Reserva.objects.all()
+    #reserva = Reserva.objects.all()
     militares = Militar.objects.raw('SELECT id, nome_de_guerra FROM registro_militar')
     armamento = Armamento.objects.raw('SELECT id, (id||" - "||modelo||" - "||fabricante||" - "||numero_de_serie) as name FROM registro_armamento')
     municao = Municao.objects.all()
@@ -136,7 +137,6 @@ def cautelar_index(request):
     context = {
         'militares': militares,
         'militar_resp': militar_resp,
-        'reserva': reserva,
         'armamento': armamento,
         'municao': municao,
         'acessorio': acessorio,
@@ -145,9 +145,83 @@ def cautelar_index(request):
     template = loader.get_template('registro/cautelar.html')
     return HttpResponse(template.render(context, request))
 
+@transaction.atomic
 def formularioCautela(request):
-    post = request.POST
-    cautela_armamento = Cautela_Armamento()
-    cautela_municao = Cautela_Armamento()
-    cautela_acessorio = Cautela_Acessorio()
+    #salva ponto para possível rollback later
+    sid = transaction.savepoint()
 
+    post = request.POST
+    #return HttpResponse(post.get('cautela_armamento_id', ''))
+
+    #foreign Models
+    militar_cautela = Militar.objects.get(pk=post.get('cautela_militar_cautela', ''))
+    militar_resp = Militar.objects.get(pk=post.get('cautela_militar_resp_id', ''))
+
+    reserva_arma_raw = None
+    if post.get('cautela_armamento_id', '') != '':
+        armamento = Armamento.objects.get(pk=post.get('cautela_armamento_id', ''))
+
+        cautela_armamento = Cautela_Armamento()
+        cautela_armamento.data_de_retirada = post.get('cautela_data_retirada', '') +' '+ post.get('cautela_horario_retirada', '')
+        cautela_armamento.militar = militar_cautela
+        cautela_armamento.armamento = armamento
+        cautela_armamento.quantidade = int(post.get('cautela_armamento_quantidade', ''))
+
+        reserva_arma_raw = Reserva.objects.raw("SELECT id FROM registro_reserva_armamento WHERE armamento_id = "+str(post.get('cautela_armamento_id', '')))
+
+        if list(reserva_arma_raw):
+            r_reserva_arma = reserva_arma_raw[0]
+            reserva_armamento = Reserva_Armamento.objects.get(pk=r_reserva_arma.id)
+            reserva_armamento.quantidade = int(reserva_armamento.quantidade) - int(post.get('cautela_armamento_quantidade', ''))
+            reserva_armamento.save()
+            cautela_armamento.save()
+
+    reserva_muni_raw = None
+    if post.get('cautela_municao_id','') != '':
+        municao = Municao.objects.get(pk=post.get('cautela_municao_id', ''))
+
+        cautela_municao = Cautela_Armamento()
+        cautela_municao.data_de_retirada = post.get('cautela_data_retirada', '') + ' '+post.get('cautela_horario_retirada', '')
+        cautela_municao.militar = militar_cautela
+        cautela_municao.municao= municao
+        cautela_municao.quantidade = post.get('cautela_municao_quantidade', '')
+
+        reserva_muni_raw = Reserva.objects.raw("SELECT id FROM registro_reserva_municao WHERE municao_id ="+str(post.get('cautela_municao_id', '')))
+        if list(reserva_muni_raw):
+            r_reserva_muni = reserva_muni_raw[0]
+            reserva_municao = Reserva_Municao.objects.get(pk=r_reserva_muni.id)
+            reserva_municao.quantidade = int(reserva_municao.quantidade) - int(post.get('cautela_municao_quantidade', ''))
+            reserva_municao.save()
+            cautela_municao.save()
+
+    reserva_acesso_raw = None
+    if post.get('cautela_acessorio_id','') != '':
+        acessorio = Acessorio.objects.get(pk=post.get('cautela_acessorio_id', ''))
+
+        cautela_acessorio = Cautela_Acessorio()
+        cautela_acessorio.data_de_retirada = post.get('cautela_data_retirada', '') +' '+ post.get('cautela_horario_retirada', '')
+        cautela_acessorio.militar = militar_cautela
+        cautela_acessorio.acessorio = acessorio
+        cautela_acessorio.quantidade = post.get('cautela_acessorio_quantidade', '')
+
+        reserva_acesso_raw = Reserva.objects.raw("SELECT id FROM registro_reserva_acessorio WHERE acessorio_id ="+str(post.get('cautela_acessorio_id', '')))
+        if list(reserva_acesso_raw):
+            r_reserva_acesso = reserva_acesso_raw[0]
+            reserva_acessorio = Reserva_Acessorio.objects.get(pk=r_reserva_acesso.id)
+            reserva_acessorio.quantidade = int(reserva_acessorio.quantidade) - int(post.get('cautela_acessorio_quantidade', ''))
+            reserva_acessorio.save()
+            cautela_acessorio.save()
+
+    cautela_militar = Cautela_Militar()
+    cautela_militar.militar = militar_cautela
+    cautela_militar.militar_resp = militar_resp
+    cautela_militar.save()
+
+    transaction.savepoint_commit(sid)
+    transaction.atomic()
+    template = loader.get_template('registro/cautelar.html')
+    context = {
+        'save': "Cautela realizada com sucesso",
+    }
+    #return HtppResponseRedirect('/p_registro/cautelas')
+    return HttpResponse(template.render(context, request))
